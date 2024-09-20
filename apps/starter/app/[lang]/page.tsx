35cdee301608e077e63ae494dc112a592f61b260
shukrullojondevnu <shukrullojon.devnu@gmail.com>
2024-09-20 08:20:37 +0500

    different metadata for lang pages

--- apps/starter/app/[lang]/page.tsx
+++ apps/starter/app/[lang]/page.tsx
@@ -1,12 +1,25 @@
 import { Locale } from "@repo/config/i18n-config";
-import { Hero } from "@repo/ui/components/hero";
 import { getDictionary } from "../../get-dictionary";
+import { Metadata, ResolvingMetadata } from "next";
 
-export default async function Home({
-  params: { lang },
-}: {
+type Props = {
+  children?: React.ReactNode;
   params: { lang: Locale };
-}) {
+  searchParams: { [key: string]: string | string[] | undefined };
+};
+
+export async function generateMetadata(
+  { params, searchParams }: Props,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  const dictionary = await getDictionary(params.lang);
+  return {
+    title: dictionary?.metadata?.title,
+    openGraph: {},
+  };
+}
+
+export default async function Home({ params: { lang } }: Props) {
   const dictionary = await getDictionary(lang);
   return <div className="container">{dictionary.test}</div>;
 }
