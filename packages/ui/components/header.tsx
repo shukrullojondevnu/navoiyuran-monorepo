a6e6355af520909ad5a88f370c84cf9b4ca05ac2
shukrullojondevnu <shukrullojon.devnu@gmail.com>
2024-09-29 09:31:38 +0500

    commit

--- packages/ui/components/header.tsx
+++ packages/ui/components/header.tsx
@@ -2,7 +2,6 @@
 import * as React from "react";
 import Link from "next/link";
 import { MoonIcon, SunIcon } from "@radix-ui/react-icons";
-import { useTheme } from "next-themes";
 import { cn } from "@repo/ui/lib/utils";
 import {
   NavigationMenu,
@@ -21,10 +20,10 @@ import {
 } from "@repo/ui/components/ui/dropdown-menu";
 import { Button } from "@repo/ui/components/ui/button";
 import LocaleSwitcher from "@repo/ui/components/locale-switcher";
+import ThemeSwitcher from "@repo/ui/components/theme-switcher";
 
 const Header = React.forwardRef<React.ElementRef<"header">, any>(
   ({ className, currentLocale, dictionary, ...props }, ref) => {
-    const { setTheme } = useTheme();
     return (
       <header ref={ref} className={cn("", className)}>
         <div className="container mx-auto flex justify-between items-center">
@@ -71,26 +70,7 @@ const Header = React.forwardRef<React.ElementRef<"header">, any>(
           </div>
           <div className="flex items-center justify-end space-x-2">
             <LocaleSwitcher currentLocale={currentLocale} />
-            <DropdownMenu>
-              <DropdownMenuTrigger asChild>
-                <Button variant="outline" size="icon">
-                  <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-                  <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-                  <span className="sr-only">Toggle theme</span>
-                </Button>
-              </DropdownMenuTrigger>
-              <DropdownMenuContent align="end">
-                <DropdownMenuItem onClick={() => setTheme("light")}>
-                  Light
-                </DropdownMenuItem>
-                <DropdownMenuItem onClick={() => setTheme("dark")}>
-                  Dark
-                </DropdownMenuItem>
-                <DropdownMenuItem onClick={() => setTheme("system")}>
-                  System
-                </DropdownMenuItem>
-              </DropdownMenuContent>
-            </DropdownMenu>
+            <ThemeSwitcher />
           </div>
         </div>
       </header>
